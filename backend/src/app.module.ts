1917c40d65a1696dd759ed7fc380d4ea51a199da
Lurldgbodex <professionaleditor6@gmail.com>
2025-04-13 19:03:24 +0100

    set up core modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TasksModule, AnalyticsModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
