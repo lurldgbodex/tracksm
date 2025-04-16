6594cc1aedcb25e04606f582c225208f67a9eff8
Lurldgbodex <professionaleditor6@gmail.com>
2025-04-16 19:17:12 +0100

    chore: configure swagger and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager')
+    .setDescription('Task Manager API description')
+    .setVersion('1.0')
+    .addTag('Tasks')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
